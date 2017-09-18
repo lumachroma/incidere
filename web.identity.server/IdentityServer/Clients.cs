86443284706958a549e6ec506aa8b926f18d4846
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-09-18 19:21:27 +0800

    Add Clients

---
+++ web.identity.server/IdentityServer/Clients.cs
@@ -0,0 +1,29 @@
+﻿using IdentityServer3.Core.Models;
+using System.Collections.Generic;
+
+namespace web.identity.server.IdentityServer
+{
+    public static class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    Enabled = true,
+                    ClientName = "MVC Client",
+                    ClientId = "mvc",
+                    Flow = Flows.Implicit,
+
+                    RedirectUris = new List<string>
+                    {
+                        "http://localhost:50451/"
+                    },
+
+                    AllowAccessToAllScopes = true
+                }
+            };
+        }
+    }
+}
