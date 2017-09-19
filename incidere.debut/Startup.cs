f86f508a41fd07610cdf503d55218c77e8cea02a
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-09-19 17:27:41 +0800

    Cleanup Startup

--- incidere.debut/Startup.cs
+++ incidere.debut/Startup.cs
@@ -1,4 +1,5 @@
-﻿using IdentityServer3.AccessTokenValidation;
+﻿using IdentityModel.Client;
+using IdentityServer3.AccessTokenValidation;
 using IdentityServer3.Core;
 using Microsoft.IdentityModel.Protocols;
 using Microsoft.Owin;
@@ -6,8 +7,10 @@ using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OpenIdConnect;
 using Owin;
+using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web.Helpers;
@@ -32,45 +35,34 @@ namespace incidere.debut
             {
                 Authority = "http://localhost:50450/identity",
                 ClientId = "mvc_incidere",
-                Scope = "openid profile roles",
+                Scope = "openid profile roles sampleApi",
                 RedirectUri = "http://localhost:50451/",
-                ResponseType = "id_token",
+                ResponseType = "id_token token",
                 SignInAsAuthenticationType = "Cookies",
 
                 Notifications = new OpenIdConnectAuthenticationNotifications
                 {
-                    SecurityTokenValidated = n =>
+                    SecurityTokenValidated = async n =>
                     {
-                        var id = n.AuthenticationTicket.Identity;
-
-                        var name = id.FindFirst(Constants.ClaimTypes.Name);
-                        var givenName = id.FindFirst(Constants.ClaimTypes.GivenName);
-                        var familyName = id.FindFirst(Constants.ClaimTypes.FamilyName);
-                        var sub = id.FindFirst(Constants.ClaimTypes.Subject);
-                        var roles = id.FindAll(Constants.ClaimTypes.Role);
-
                         var nid = new ClaimsIdentity(
-                            id.AuthenticationType,
+                            n.AuthenticationTicket.Identity.AuthenticationType,
                             Constants.ClaimTypes.GivenName,
                             Constants.ClaimTypes.Role);
 
-                        if (null != name)
-                            nid.AddClaim(name);
-                        if (null != givenName)
-                            nid.AddClaim(givenName);
-                        if (null != familyName)
-                            nid.AddClaim(familyName);
-                        nid.AddClaim(sub);
-                        nid.AddClaims(roles);
+                        var userInfoClient = new UserInfoClient(
+                            new Uri(n.Options.Authority + "/connect/userinfo"),
+                            n.ProtocolMessage.AccessToken);
+
+                        var userInfo = await userInfoClient.GetAsync();
+
+                        userInfo.Claims.ToList().ForEach(ui => nid.AddClaim(new Claim(ui.Item1, ui.Item2)));
+                        nid.AddClaim(new Claim("access_token", n.ProtocolMessage.AccessToken));
+                        nid.AddClaim(new Claim("expires_at", DateTimeOffset.Now.AddSeconds(int.Parse(n.ProtocolMessage.ExpiresIn)).ToString()));
+                        nid.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
+                        nid.AddClaim(new Claim("sid", n.AuthenticationTicket.Identity.FindFirst("sid").Value));
                         nid.AddClaim(new Claim("app_specific", "some data"));
 
-                        nid.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
-
-                        n.AuthenticationTicket = new AuthenticationTicket(
-                            nid,
-                            n.AuthenticationTicket.Properties);
-
-                        return Task.FromResult(0);
+                        n.AuthenticationTicket = new AuthenticationTicket(nid, n.AuthenticationTicket.Properties);
                     },
 
                     RedirectToIdentityProvider = n =>
@@ -78,13 +70,11 @@ namespace incidere.debut
                         if (n.ProtocolMessage.RequestType == OpenIdConnectRequestType.LogoutRequest)
                         {
                             var idTokenHint = n.OwinContext.Authentication.User.FindFirst("id_token");
-
                             if (idTokenHint != null)
                             {
                                 n.ProtocolMessage.IdTokenHint = idTokenHint.Value;
                             }
                         }
-
                         return Task.FromResult(0);
                     }
                 }
--- web.identity.server/Startup.cs
+++ web.identity.server/Startup.cs
@@ -76,29 +76,20 @@ namespace web.identity.server
                             Constants.ClaimTypes.GivenName,
                             Constants.ClaimTypes.Role);
 
-                        // get userinfo data
                         var userInfoClient = new UserInfoClient(
                             new Uri(n.Options.Authority + "/connect/userinfo"),
                             n.ProtocolMessage.AccessToken);
 
                         var userInfo = await userInfoClient.GetAsync();
+
                         userInfo.Claims.ToList().ForEach(ui => nid.AddClaim(new Claim(ui.Item1, ui.Item2)));
-
-                        // keep the id_token for logout
-                        nid.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
-
-                        // add access token for sample API
                         nid.AddClaim(new Claim("access_token", n.ProtocolMessage.AccessToken));
-
-                        // keep track of access token expiration
                         nid.AddClaim(new Claim("expires_at", DateTimeOffset.Now.AddSeconds(int.Parse(n.ProtocolMessage.ExpiresIn)).ToString()));
-
-                        // add some other app specific claim
+                        nid.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
+                        nid.AddClaim(new Claim("sid", n.AuthenticationTicket.Identity.FindFirst("sid").Value));
                         nid.AddClaim(new Claim("app_specific", "some data"));
 
-                        n.AuthenticationTicket = new AuthenticationTicket(
-                            nid,
-                            n.AuthenticationTicket.Properties);
+                        n.AuthenticationTicket = new AuthenticationTicket(nid, n.AuthenticationTicket.Properties);
                     },
 
                     RedirectToIdentityProvider = n =>
@@ -106,13 +97,11 @@ namespace web.identity.server
                         if (n.ProtocolMessage.RequestType == OpenIdConnectRequestType.LogoutRequest)
                         {
                             var idTokenHint = n.OwinContext.Authentication.User.FindFirst("id_token");
-
                             if (idTokenHint != null)
                             {
                                 n.ProtocolMessage.IdTokenHint = idTokenHint.Value;
                             }
                         }
-
                         return Task.FromResult(0);
                     }
                 }
