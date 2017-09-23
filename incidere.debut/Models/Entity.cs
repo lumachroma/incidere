f0978e40f999ddf209872526945109f2dd4a2be4
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-09-23 15:48:41 +0800

    Create base class Entity and Entity Collection

---
+++ incidere.debut/Models/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace incidere.debut.Models
+{
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            DoWork();
+        }
+
+        public string ChangedBy { get; set; }
+        public DateTime ChangedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Id { get; set; }
+        public string WebId { get; set; }
+        public string FirebaseKey { get; set; }
+
+        public abstract void DoWork();
+    }
+}
---
+++ incidere.debut/Models/EntityCollection.cs
@@ -0,0 +1,14 @@
+﻿namespace incidere.debut.Models
+{
+    public abstract class EntityCollection
+    {
+        public EntityCollection()
+        {
+            DoWork();
+        }
+
+        public string WebId { get; set; }
+
+        public abstract void DoWork();
+    }
+}
