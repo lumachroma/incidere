6e9f133003cb155bae6dd8b25b22f875c2072bf3
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-09-23 17:19:27 +0800

    Create Entity LocalUser

---
+++ incidere.debut/Models/LocalUser/ExternalUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace incidere.debut.Models.LocalUser
+{
+    public class ExternalUser : EntityCollection
+    {
+
+        public ExternalUser()
+        {
+        }
+
+        public string Provider { get; set; }
+        public string ProviderId { get; set; }
+        public List<string> Roles { get; } = new List<string>();
+
+        public override void DoWork()
+        {
+            WebId = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ incidere.debut/Models/LocalUser/LocalUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace incidere.debut.Models.LocalUser
+{
+    public class LocalUser : Entity
+    {
+        public LocalUser()
+        {
+            this.ReferenceNo = Guid.NewGuid().ToString();
+        }
+
+        public string ReferenceNo { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Location { get; set; }
+        public List<ExternalUser> ExternalUsers { get; } = new List<ExternalUser>();
+        public List<string> Roles { get; } = new List<string>();
+
+        public override string ToString()
+        {
+            return "LocalUsers:" + ReferenceNo;
+        }
+
+        public override void DoWork()
+        {
+            ChangedBy = string.Empty;
+            ChangedDate = DateTime.Now;
+            CreatedBy = string.Empty;
+            CreatedDate = DateTime.Now;
+            Id = Guid.NewGuid().ToString();
+            WebId = Guid.NewGuid().ToString();
+        }
+    }
+}
