fe9472b102f7577eef55c208033021df36878c02
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-09-23 23:44:03 +0800

    Create Entity Setting (in internal entity models)

---
+++ incidere.debut/Models/Internals/Settings/Setting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace incidere.debut.Models.Internals.Settings
+{
+    public class Setting : Entity
+    {
+        public Setting()
+        {
+        }
+
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string Subject { get; set; }
+        public string Text { get; set; }
+
+        public override string ToString()
+        {
+            return "Setting:" + Key;
+        }
+
+        public override void DoWork()
+        {
+            ChangedBy = string.Empty;
+            ChangedDate = DateTime.Now;
+            CreatedBy = string.Empty;
+            CreatedDate = DateTime.Now;
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
