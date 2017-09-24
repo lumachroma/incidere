63990f1583e9c123b32ffada1c1daf9666582700
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-09-24 12:12:34 +0800

    [web.identity.server] Create Incidere User Services

---
+++ web.identity.server/Services/IncidereUserService.cs
@@ -0,0 +1,49 @@
+﻿using incidere.debut.Models.LocalUser;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+
+namespace web.identity.server.Services
+{
+    public class IncidereUserService
+    {
+        private string m_incidereServiceUrl;
+        private HttpClient m_incidereServiceClient;
+
+        public IncidereUserService()
+        {
+            m_incidereServiceUrl = ConfigurationManager.AppSettings["IncidereBaseUrl"] ?? "http://localhost:50451";
+            m_incidereServiceClient = new HttpClient { BaseAddress = new Uri(m_incidereServiceUrl) };
+        }
+
+        public List<LocalUser> GetUsers()
+        {
+            var firebaseUsers = new List<LocalUser>();
+            try
+            {
+                var output = m_incidereServiceClient.GetStringAsync("api/local-users").Result;
+                try
+                {
+                    var json = JObject.Parse(output).SelectToken("$._results");
+                    foreach (var jtok in json)
+                    {
+                        var firebaseUser = jtok.ToObject<LocalUser>();
+                        firebaseUsers.Add(firebaseUser);
+                    }
+                }
+                catch (Exception)
+                {
+                    return firebaseUsers;
+                }
+            }
+            catch (Exception)
+            {
+                return firebaseUsers;
+            }
+
+            return firebaseUsers;
+        }
+    }
+}
