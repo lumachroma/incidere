9fddee4275ad2d294af59df6f0949893f2fda6d4
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-09-24 12:13:38 +0800

    [web.identity.server] Create Incidere User Controller and View

---
+++ web.identity.server/Controllers/CustomIncidereUserController.cs
@@ -0,0 +1,95 @@
+﻿using System.Web.Mvc;
+using web.identity.server.Services;
+
+namespace web.identity.server.Controllers
+{
+    [Authorize]
+    public class CustomIncidereUserController : Controller
+    {
+        private IncidereUserService m_incidereUserService;
+
+        public CustomIncidereUserController()
+        {
+            m_incidereUserService = new IncidereUserService();
+        }
+
+        // GET: CustomIncidereUser
+        public ActionResult Index()
+        {
+            var incidereUsers = m_incidereUserService.GetUsers();
+            return View(incidereUsers);
+        }
+
+        // GET: CustomIncidereUser/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomIncidereUser/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomIncidereUser/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomIncidereUser/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomIncidereUser/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomIncidereUser/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomIncidereUser/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
