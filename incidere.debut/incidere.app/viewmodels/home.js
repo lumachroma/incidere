f1b32d6cc9c3c064c73661df4d839739e9787301
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-10-09 18:12:12 +0800

    [incidere.debut] Using custom config from Durandal

--- incidere.debut/incidere.app/viewmodels/home.js
+++ incidere.debut/incidere.app/viewmodels/home.js
@@ -1,8 +1,8 @@
-﻿define([], function () {
+﻿define(["services/config"], function (config) {
 
     return {
         activate: function () {
-
+            console.log(config.application_name);
         },
         attached: function () {
 
