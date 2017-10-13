1ab6291bc68e724d64a20da29fa480df2bb00daf
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-10-13 19:15:11 +0800

    [incidere.debut] Show username in navbar and create button dropdown for user related features

--- incidere.debut/incidere.admin/viewmodels/shell.js
+++ incidere.debut/incidere.admin/viewmodels/shell.js
@@ -1,6 +1,7 @@
-﻿define(['plugins/router'], function (router) {
+﻿define(['plugins/router', 'services/config'], function (router, config) {
     return {
         router: router,
+        config: config,
         activate: function () {
             router.map([
                 { route: '', title: 'Dashboard', moduleId: 'viewmodels/dashboard', nav: true },
