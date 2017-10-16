fedfc1a6fded3e31425173cbd7144aad1af192e2
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-10-16 13:31:55 +0800

    [incidere.debut] create new routes: profile, setting; in durandal app (for admin)

--- incidere.debut/incidere.admin/viewmodels/shell.js
+++ incidere.debut/incidere.admin/viewmodels/shell.js
@@ -1,10 +1,12 @@
 ﻿define(['plugins/router', 'services/config'], function (router, config) {
     return {
         router: router,
-        config: config,
+        username: config.preferred_username,
         activate: function () {
             router.map([
                 { route: '', title: 'Dashboard', moduleId: 'viewmodels/dashboard', nav: true },
+                { route: 'profile', title: 'Profile', moduleId: 'viewmodels/profile', nav: true },
+                { route: 'setting', title: 'Setting', moduleId: 'viewmodels/setting', nav: true },
                 { route: 'playground', title: 'Playground', moduleId: 'viewmodels/playground', nav: true }
             ]).buildNavigationModel();
             return router.activate();
