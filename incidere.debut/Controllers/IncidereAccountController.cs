bd42865004f7f5f6470c5999f1f3f12407d9e77d
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-10-17 17:31:37 +0800

    [incidere.debut] Refactor IncidereAccountController

--- incidere.debut/Controllers/IncidereAccountController.cs
+++ incidere.debut/Controllers/IncidereAccountController.cs
@@ -6,48 +6,40 @@ using System.Web.Mvc;
 
 namespace incidere.debut.Controllers
 {
-    [Authorize(Roles = "Admin, Administrator")]
     [RoutePrefix("incidere-account")]
     public class IncidereAccountController : Controller
     {
         private IncidereUserService m_incidereUserService;
+        private IncidereMailService m_incidereMailService;
 
         public IncidereAccountController()
         {
             m_incidereUserService = new IncidereUserService();
+            m_incidereMailService = new IncidereMailService();
         }
 
+        [Authorize]
         [Route("change-password/{id}")]
         [HttpPost]
-        public ActionResult ChangePassword(ChangePasswordModel passwordModel, string id)
+        public ActionResult ChangePassword(IncidereAccountChangePasswordModel model, string id)
         {
             var resultSuccess = true;
             var resultStatus = "OK";
 
-            if (!(passwordModel.NewPassword == passwordModel.ConfirmPassword)
-                || !(passwordModel.ConfirmPassword.Length >= 8))
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
             if (string.IsNullOrEmpty(id))
-            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+
+            if (!ValidatePasswords(model.NewPassword, model.ConfirmPassword))
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
             var localUser = m_incidereUserService.GetUser(id);
             if (string.IsNullOrEmpty(localUser.FirebaseKey))
-            {
                 return new HttpStatusCodeResult(HttpStatusCode.NotFound);
-            }
 
-            var verify = Crypto.VerifyHashedPassword(localUser.Password, passwordModel.OldPassword);
-            if (!verify)
-            {
+            if (!VerifyPassword(localUser.Password, model.OldPassword))
                 return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
-            }
 
-            localUser.Password = Crypto.HashPassword(passwordModel.ConfirmPassword);
+            localUser.Password = CreateHashedPassword(model.ConfirmPassword);
             try
             {
                 var result = m_incidereUserService.EditUser(localUser, id);
@@ -63,9 +55,29 @@ namespace incidere.debut.Controllers
             Response.StatusCode = (int)HttpStatusCode.OK;
             return Json(new { success = resultSuccess, status = resultStatus, id = id });
         }
+
+        private static bool ValidatePasswords(string newPassword, string confirmPassword)
+        {
+            // TODO: validate password strength
+            if ((newPassword == confirmPassword) && (confirmPassword.Length >= 8))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool VerifyPassword(string hashedPassword, string password)
+        {
+            return Crypto.VerifyHashedPassword(hashedPassword, password);
+        }
+
+        private static string CreateHashedPassword(string password)
+        {
+            return Crypto.HashPassword(password);
+        }
     }
 
-    public class ChangePasswordModel
+    public class IncidereAccountChangePasswordModel
     {
         public string OldPassword { get; set; }
         public string NewPassword { get; set; }
