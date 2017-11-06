115dc7df714b315b55f24875dd2d8f5cb3cb1f42
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-11-07 00:10:33 +0800

    [incidere.denut] implement local-user form validation

--- incidere.debut/incidere.admin/viewmodels/local-user-details.js
+++ incidere.debut/incidere.admin/viewmodels/local-user-details.js
@@ -1,27 +1,30 @@
-﻿define(['knockout', 'schemas', 'durandal/system', 'durandal/app', 'plugins/router', 'services/config', 'services/datacontext'],
+﻿define(['knockout', 'schemas', 'durandal/system', 'durandal/app',
+    'plugins/router', 'services/config', 'services/datacontext'],
     function (ko, schemas, system, app, router, config, context) {
         var isBusy = ko.observable(false),
             id = ko.observable(),
             showEditForm = ko.observable(false),
             entity = ko.observable(new schemas.LocalUser(system.guid())),
+            usernameExist = ko.observable(false),
+            emailExist = ko.observable(false),
             activate = function (entityId) {
                 id(entityId);
                 if (!entityId || entityId === "0") {
-                    entity(new schemas.LocalUser(system.guid()));
+                    entity(schemas.LocalUser(system.guid()));
                     showEditForm(true);
                 } else {
                     isBusy(true);
-                    context.get(`/api/local-users/${id()}`, true, {})
+                    return context.get(`/api/local-users/${id()}`, true, {})
                         .done(function (result) {
                             //console.log(result);
-                            entity(new schemas.LocalUser(result._result));
+                            entity(schemas.LocalUser(result._result));
                             console.log(ko.toJSON(entity));
                             isBusy(false);
                         }).fail(function (e) {
-                            console.log(e.status);
-                            console.log(e.statusText);
+                            console.log(`${e.status} ${e.statusText} ${e.responseJSON.status}`);
                             isBusy(false);
-                            app.showMessage(`${e.status}: ${e.statusText}. Invalid Entity Id: ${id()}`, config.application_name, ["OK"])
+                            app.showMessage(`${e.status}: ${e.statusText}. Invalid Entity Id: ${id()}`,
+                                config.application_name, ["OK"])
                                 .done(function (result) {
                                     if (result == "OK") {
                                         router.navigate("local-users-all");
@@ -31,6 +34,14 @@
                 }
             },
             defultOperationEndpoint = function (json, endpoint, verb, successMessage) {
+                if (!$('#local-user-details-form').valid()) {
+                    console.log("#local-user-details-form invalid");
+                    return;
+                }
+                if (usernameExist() || emailExist()) {
+                    console.log("username/email invalid");
+                    return;
+                }
                 isBusy(true);
                 context.send(json, `/${endpoint}`, verb)
                     .done(function (result) {
@@ -75,6 +86,29 @@
                         defultOperationEndpoint(data, endpoint, "DELETE", msg);
                     });
             },
+            checkPropertyExist = function (endpoint, property) {
+                isBusy(true);
+                context.get(`/${endpoint}`, false, {})
+                    .done(function (result) {
+                        //console.log(result._result);
+                        if (result._status.success) {
+                            property(true);
+                        }
+                        isBusy(false);
+                    }).fail(function (e) {
+                        //console.log(`${e.status} ${e.statusText} ${e.responseJSON.status}`);
+                        property(false);
+                        isBusy(false);
+                    });
+            },
+            checkUsernameExist = function (newUsername) {
+                var endpoint = `api/local-users/Username/${newUsername}/`;
+                checkPropertyExist(endpoint, usernameExist);
+            },
+            checkEmailExist = function (newEmail) {
+                var endpoint = `api/local-users/Email/${newEmail}/`;
+                checkPropertyExist(endpoint, emailExist);
+            },
             toggleShowEditForm = function () {
                 showEditForm(!showEditForm());
             },
@@ -82,19 +116,42 @@
                 router.navigate("local-users-all");
             },
             attached = function () {
-
+                $('#local-user-details-form').validate({
+                    errorClass: "is-invalid",
+                    rules: {
+                        Username: { required: true },
+                        Email: { required: true, email: true },
+                        FirstName: { required: true },
+                        LastName: { required: true },
+                        DateOfBirth: { required: true },
+                        Location: { required: true }
+                    },
+                    messages: {
+                        Username: { required: "Username is required" },
+                        Email: { required: "Email is required" },
+                        FirstName: { required: "FirstName is required" },
+                        LastName: { required: "LastName is required" },
+                        DateOfBirth: { required: "DateOfBirt is required" },
+                        Location: { required: "Location is required" }
+                    }
+                });
             },
             compositionComplete = function () {
-
+                entity().Username.subscribe(checkUsernameExist);
+                entity().Email.subscribe(checkEmailExist);
             },
             deactivate = function () {
                 id(null);
+                usernameExist(false);
+                emailExist(false);
                 showEditForm(false);
             };
 
         return {
             id: id,
             entity: entity,
+            usernameExist: usernameExist,
+            emailExist: emailExist,
             editEntity: editEntity,
             addEntity: addEntity,
             deleteEntity: deleteEntity,
